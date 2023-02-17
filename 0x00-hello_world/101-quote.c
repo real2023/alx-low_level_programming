f1e342cb0fb5f2804318f853bc3baced4b5eaf39
real2023 <adewuyitaiwoebenezer@gmail.com>
2023-02-16 23:57:56 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - print a string and standard error
  *
@@ -7,5 +9,5 @@
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	return (1);
 }
