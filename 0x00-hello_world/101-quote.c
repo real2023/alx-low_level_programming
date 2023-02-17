d320995c4b5af2f0bee2b4d31ab48f23f3fa1a3e
real2023 <adewuyitaiwoebenezer@gmail.com>
2023-02-16 23:42:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
-/**                                                   
-* main - prints a string and standard error
- * Return: 1                                          
-*/
-int main(void)                                       {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+/**
+ * main - print a string and standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
